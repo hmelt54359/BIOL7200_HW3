8adfef78353146a7c1f966787925f2d2a723f723
Hayden Melton <hayden.melton54359@gmail.com>
2025-09-08 02:44:10 -0400

    use correct updated script

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-query=(realpath "$1")
-subject=(realpath "$2")
-output=(realpath "$3")
-tblastn -query "$query" -subject "$subject" -outfmt "6 pident length qlen" | awk '$1 > 30 && ($2/$3) > 0.9 {print $0}' > "$output"
+query=$1
+subject=$2
+output=$3
+tblastn -query "$query" -subject "$subject" -outfmt "6 pident qlen length" | awk '$1 > 30 && $3 / $2 > 0.90' > "$output"
 wc -l < "$output"
